b6159596ac79fa2182ada7d159ee3d7d570d9387
Daniela Gardinalli <daniela.gardinalli@gmail.com>
2019-07-02 16:29:48 -0300

    criar funcao de str

--- app/models.py
+++ app/models.py
@@ -36,3 +36,6 @@ class Vilao(models.Model):
     nv_maldade = models.PositiveIntegerField(choices=nivel_valor),
     vivo = models.BooleanField(default=True)
     foto = models.ImageField(upload_to='')
+
+    def __str__(self)
+        return self.nome
